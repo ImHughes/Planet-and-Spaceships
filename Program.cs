d66bccd30cea7810b991cc339c072aab3290e04d
Hughes Speer <ImHughes@users.noreply.github.com>
2019-07-09 08:54:28 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planetsAndSpaceships
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            //Add() Jupiter and Saturn at the end of the list.
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            planetList.Insert(1, "Earth");
+            planetList.Insert(1, "Venus");
+
+            List<string> newPlanetList = new List<string>() { "Uranus", "Neptune" };
+            planetList.AddRange(newPlanetList);
+            planetList.Add("Pluto");
+            planetList.Remove("Pluto");
+            planetList.ForEach(planetElement => Console.WriteLine(planetElement));
+            // var rockyPlanets = planetList.GetRange(0, 4);
+            // rockyPlanets.ForEach(rockyElement => Console.WriteLine(rockyElement));
+            Console.WriteLine(planetList);
+
+        }
+    }
+}
